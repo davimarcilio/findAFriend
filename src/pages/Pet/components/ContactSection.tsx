dcf73fac0cb99398992cae182b2f11eaa4f84d4c
Davi Marcilio <davimarcilio.js@gmail.com>
2023-05-23 10:52:39 -0300

    :boom: fix: Adicionando link dinâmico

--- src/pages/Pet/components/ContactSection.tsx
+++ src/pages/Pet/components/ContactSection.tsx
@@ -1,11 +1,16 @@
+import { CompletePet } from '@/models/interfaces/Pet'
 import { WhatsappLogo } from 'phosphor-react'
 
-export function ContactSection() {
+interface ContactSectionProps {
+  currentPet: CompletePet
+}
+
+export function ContactSection({ currentPet }: ContactSectionProps) {
   return (
     <section id="contact">
       <a
         className="text-white flex justify-center items-center py-5 w-full rounded-3xl bg-green-400 gap-4"
-        href={`https://api.whatsapp.com/send?phone=5548991256373&text=Ol%C3%A1%20vim%20pelo%20aplicativo%20Find%20A%20Friend`}
+        href={`https://api.whatsapp.com/send?phone=${currentPet.org.whatsappNumber}&text=Olá vim pelo aplicativo Find A Friend, Gostaria de saber mais informações sobre o PET: ${currentPet.name}`}
       >
         <WhatsappLogo weight="bold" size={24} />
         <p className="text-lg font-extrabold">Entrar em contato</p>
