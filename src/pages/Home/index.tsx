ad7687eec9bfa1de34ff9379649a4d3a69bc7ac7
Davi Marcilio <davimarcilio.js@gmail.com>
2023-05-25 20:23:16 -0300

    :sparkles: feat: Login page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,25 @@
 import dogsHome from '../../assets/images/dogsHome.png'
 import Logo from '../../assets/icons/logo.svg'
 import { Form } from './components/Form'
+import { Link } from 'react-router-dom'
 
 export function Home() {
   return (
     <main className="w-screen h-screen bg-red-500 flex justify-center px-5 items-center">
       <div className="max-w-7xl max-h-[572px] max-lg:max-h-none py-10 h-full max-lg:gap-6 flex justify-between max-sm:justify-center items-center flex-col">
-        <header className="flex self-start justify-center items-center gap-3">
-          <img className="h-14" src={Logo} alt="" />
-          <h1 className="font-bold text-xl">FindAFriend</h1>
+        <header className="flex w-full justify-between items-center">
+          <div className="flex self-start justify-center items-center gap-3">
+            <img className="h-14" src={Logo} alt="" />
+            <h1 className="font-bold text-xl">FindAFriend</h1>
+          </div>
+          <div className="flex self-start justify-center items-center gap-3">
+            <Link
+              to={'/login'}
+              className="text-slate-800 font-bold bg-yellow-500 p-2 rounded-lg flex justify-center items-center hover:opacity-80 transition-all"
+            >
+              Entrar
+            </Link>
+          </div>
         </header>
         <section className="flex justify-between max-lg:flex-col items-center max-lg:gap-6 w-full">
           <h1 className="font-extrabold text-7xl max-sm:text-5xl leading-[90%] max-lg:self-start self-end max-w-lg max-lg:max-w-none">
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { Map } from '../pages/Map'
 import { Pet } from '@/pages/Pet'
+import { Login } from '@/pages/Auth/Login'
+import { Register } from '@/pages/Auth/Register'
 
 export function Router() {
   return (
@@ -9,6 +11,8 @@ export function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/map" element={<Map />} />
       <Route path="/pet/:id" element={<Pet />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
