a7bd97f0c7da79d72f33d659a2e6f84160965ab6
Davi Marcilio <davimarcilio.js@gmail.com>
2023-05-28 13:42:31 -0300

    :sparkles: feat: React hook form added to LoginForm

--- src/pages/Auth/Login/components/LoginForm.tsx
+++ src/pages/Auth/Login/components/LoginForm.tsx
@@ -1,14 +1,63 @@
 import { Button } from '@/components/Button'
 import { Link } from 'react-router-dom'
 import { InputForm } from '../../components/InputForm'
+import { useForm } from 'react-hook-form'
+
+interface FormData {
+  email?: string
+  password?: string
+}
 
 export function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>()
+
+  function onSubmit(data) {
+    console.log(data)
+  }
+
   return (
-    <form className="flex flex-col gap-16 text-blue-900 max-w-lg flex-1">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-16 text-blue-900 max-w-lg flex-1"
+    >
       <h1 className="text-6xl font-bold">Boas-vindas!</h1>
       <div className="flex flex-col gap-4">
-        <InputForm label="Email" placeholder="nome@email.com" />
-        <InputForm type="password" label="Senha" placeholder="*********" />
+        <InputForm
+          {...register('email', {
+            required: {
+              value: true,
+              message: 'O email é obrigatório',
+            },
+          })}
+          label="Email"
+          type="email"
+          placeholder="nome@email.com"
+          errorMessage={errors.email?.message}
+        />
+        <InputForm
+          {...register('password', {
+            minLength: {
+              value: 8,
+              message: 'A senha deve conter no minímo 8 caracteres',
+            },
+            maxLength: {
+              value: 30,
+              message: 'A senha deve conter no máximo 30 caracteres',
+            },
+            required: {
+              value: true,
+              message: 'A senha é obrigatório',
+            },
+          })}
+          type="password"
+          label="Senha"
+          placeholder="*********"
+          errorMessage={errors.password?.message}
+        />
       </div>
       <div className="flex flex-col gap-4">
         <Button
