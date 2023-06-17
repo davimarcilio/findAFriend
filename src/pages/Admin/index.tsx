3b959f46b2bf6b6e6a7124f64a834a18eb7a745b
Davi Marcilio <davimarcilio.js@gmail.com>
2023-06-17 13:13:45 -0300

    :sparkles: feat: New button redirect to /map page

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -4,14 +4,10 @@ import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '@/assets/icons/logo.svg'
 import { Plus } from 'phosphor-react'
+import { OrgCard } from './components/OrgCard'
 export function Admin() {
   const { token } = useContext(OrgContext)
-  // const navigate = useNavigate()
-  // useEffect(() => {
-  //   if (!token) {
-  //     navigate('/')
-  //   }
-  // }, [token])
+
   if (!token) {
     return (
       <main className="flex flex-col w-screen h-screen justify-center items-center">
@@ -26,6 +22,9 @@ export function Admin() {
         <h1 className="text-lg font-semibold text-blue-100">
           Administrar sua organização
         </h1>
+        <div className="max-w-3xl w-full">
+          <OrgCard />
+        </div>
         <div className="grid grid-cols-3 max-md:grid-cols-1 gap-10">
           <Link
             className="flex justify-center items-center p-10 font-extrabold text-2xl rounded-3xl bg-red-500 h-fit mt-10 gap-5 hover:opacity-70 transition-all"
@@ -33,6 +32,13 @@ export function Admin() {
           >
             <img src={Logo} alt="" />
             Cadastrar novo PET
+          </Link>
+          <Link
+            className="flex justify-center items-center p-10 font-extrabold text-2xl rounded-3xl bg-red-500 h-fit mt-10 gap-5 hover:opacity-70 transition-all"
+            to={'/map'}
+          >
+            <img src={Logo} alt="" />
+            Ver PETS
           </Link>
           <button
             disabled
