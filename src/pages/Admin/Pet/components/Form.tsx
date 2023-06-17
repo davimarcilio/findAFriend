c2d2fe02b329065a10226edb59fbcb044215fb2b
Davi Marcilio <davimarcilio.js@gmail.com>
2023-06-17 13:17:44 -0300

    :recycle: refactor: Separate validator

--- src/pages/Admin/Pet/components/Form.tsx
+++ src/pages/Admin/Pet/components/Form.tsx
@@ -12,7 +12,6 @@ import { RequirementSection } from './RequirementSection'
 import { Button } from '@/components/Button'
 import { InputForm } from '@/components/InputForm'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useContext, useState } from 'react'
 import { SelectComponent } from './SelectComponent'
@@ -20,92 +19,7 @@ import { api } from '@/lib/axios'
 import { OrgContext } from '@/context/OrgContext'
 import { isAxiosError } from 'axios'
 import { AlertContext } from '@/context/AlertContext'
-
-const petFormSchemaValidator = z.object({
-  name: z
-    .string({
-      required_error: 'Nome é obrigatório',
-    })
-    .min(3, 'Nome deve ter no mínimo 3 caracteres'),
-  age: z.enum(['', ...ageOptions.slice(1).map((age) => age.value)]),
-  description: z
-    .string()
-    .min(10, 'Descrição deve ter no minímo 10 caracteres')
-    .max(300, 'Descrição deve ter no máximo 300 caracteres'),
-  energy: z
-    .string()
-    .refine(
-      (energy) => Number(energy) > 0 && Number(energy) < 6,
-      'Energia invalida',
-    ),
-  independence: z.enum([
-    '',
-    ...independencyOptions.slice(1).map((independence) => independence.value),
-  ]),
-  size: z.enum(['', ...sizeOptions.slice(1).map((size) => size.value)]),
-  type: z.enum(['', ...petOptions.slice(1).map((pet) => pet.value)]),
-  adoptionRequirements: z
-    .string({
-      required_error: 'Adicione pelo menos um requesito',
-    })
-    .refine(
-      (requirements) => JSON.parse(requirements).length > 0,
-      'Adicione pelo menos um requesito',
-    ),
-  images: z
-    .array(z.instanceof(File))
-    .min(1, 'É obrigatório ter no mínimo uma imagem')
-    .max(6, 'Não é permitido enviar mais de 6 imagens')
-    .superRefine((images, ctx) => {
-      const imagesType = images.map((image) => image.type.split('/')[0])
-
-      const everyIsImage = imagesType.every((type) => type === 'image')
-
-      if (!everyIsImage) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.invalid_type,
-          message: 'Não é permitido arquivos que não são imagens',
-          expected: 'array',
-          received: 'unknown',
-        })
-      }
-
-      function verifyDuplicate(images: File[]) {
-        const hashMap = {}
-        const duplicates = []
-
-        images.forEach((file) => {
-          const fileName = file.name
-          const fileSize = file.size
-
-          const key = `${fileName}_${fileSize}`
-
-          if (hashMap[key]) {
-            duplicates.push(file)
-          } else {
-            hashMap[key] = true
-          }
-        })
-
-        return duplicates
-      }
-
-      const duplicates = verifyDuplicate(images)
-      if (duplicates.length > 0) {
-        ctx.addIssue({
-          code: z.ZodIssueCode.custom,
-          message: 'Não pode conter imagens repetidas',
-          // path: 'images',
-        })
-      }
-    }),
-
-  //   images: z
-  //     .any()
-  //     .refine((files) => files.length > 0, 'Adicione pelo menos uma imagem!'),
-})
-
-type FormData = z.infer<typeof petFormSchemaValidator>
+import { PetFormData, petFormSchemaValidator } from '@/validator/admin/PetForm'
 
 export function Form() {
   const [requirements, setRequirements] = useState<string[]>([])
@@ -118,7 +32,7 @@ export function Form() {
     handleSubmit,
     reset,
     formState: { errors, isSubmitting },
-  } = useForm<FormData>({
+  } = useForm<PetFormData>({
     values: {
       images,
       adoptionRequirements: JSON.stringify(requirements),
@@ -138,7 +52,7 @@ export function Form() {
     name,
     size,
     type,
-  }: FormData) {
+  }: PetFormData) {
     const formData = new FormData()
 
     formData.set('age', age)
